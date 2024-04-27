e392a173e9fbadd69e2467192c9360cff0ee5578
unknown <anshgandhi28@gmail.com>
2024-04-27 15:08:01 +0530

    Todo list

--- UniVerse/urls.py
+++ UniVerse/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path,include
 from UniVerse import views
+from todolist import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('',views.home,name="home")
+    # path('',views.home,name='home')
+    path('todolist/',include('todolist.urls'))
 ]
