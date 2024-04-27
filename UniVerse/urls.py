37581458aca2ac83efb4743d0b62e6afb637122c
ARNAV JOGLEKAR <totallynotarnav@gmail.com>
2024-04-27 17:02:55 +0530

    base2

--- UniVerse/urls.py
+++ UniVerse/urls.py
@@ -3,8 +3,12 @@ from django.urls import path,include
 from UniVerse import views
 
 urlpatterns = [
+    path('',views.home,name="home"),
     path('admin/', admin.site.urls),
+
     path('accounts/', include('accounts.urls')),
-    path('',views.home,name='home'),
+
+    path('resources/', include('resources.urls')),
+
     path('todolist/',include('todolist.urls'))
 ]
