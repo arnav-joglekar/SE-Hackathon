fde997573c45a2a4413766eb17c175bf54f27f2b
unknown <anshgandhi28@gmail.com>
2024-04-27 22:35:30 +0530

    Kind of completed basic functionality of to do list of selfstudy

--- UniVerse/urls.py
+++ UniVerse/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
-from UniVerse import views
-from todolist import views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    # path('',views.home,name='home')
+    path('',views.home,name='home'),
     path('todolist/',include('todolist.urls'))
 ]
--- todolist/urls.py
+++ todolist/urls.py
@@ -7,4 +7,8 @@ app_name = 'todolist'
 urlpatterns = [
     path('', views.index, name='index'),
     path('assignments/', views.assignments, name='assignments'),
+    path('selfstudy/', views.selfstudy, name='selfstudy'),
+    path('create_assignments/', views.create_assignments, name='create_assignments'),
+    path('create_selfstudy/', views.create_selfstudy, name='create_selfstudy'),
+    path('complete/<uuid:uuid>', views.complete_selfstudy, name='complete_selfstudy'),
 ]
